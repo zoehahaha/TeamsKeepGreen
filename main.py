900323237debd7b5a083826c5e54725eb2a4464b
Zoe Zou <133292874+zoehahaha@users.noreply.github.com>
2023-12-08 11:24:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ import time
 pyautogui.FAILSAFE = FALSE
 
 while True:
-  time.sleep(10) 
+  time.sleep(10)
+
+# this for loop is used to move the mouse
+for i in range(0,100):
+  pyautogui.move(0,i*10)
+pyautogui.click(x=0,y=1000) # click the left bottom corner of the screen
+
+for i in range(0,3):
+  pyautogui.press("shift")
